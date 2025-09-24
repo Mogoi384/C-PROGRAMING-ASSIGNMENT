e0bb7f3062f368d26d4789d9ba25a899a2db557a
Mogoi384 <mogoiignatius389@gmail.com>
2025-09-24 17:03:26 +0300

    Add files via upload

---
+++ semesteroneunits.c
@@ -0,0 +1,22 @@
+//A PROGRAM TO DISPLAY UNITS FOR SEMESTER 1.1
+/*
+Name:MOGOI IGNATIUS
+Reg no:PA106/G/28834/25
+Desription:Program to display units for semester 1.1
+*/
+#include <stdio.h>
+int main()
+{
+	printf("                   SEMESTER 1.1 UNITS      \n");
+	printf("\n");
+	printf("\t1.(SPC 2101) Introduction to progamming and problem solving\n");
+	printf("\t2.(SPI 2102) Computer systems and organization\n");
+	printf("\t3.(SPM 2100) Discrete mathematics\n");
+	printf("\t4.(SPM 2104) Mathematics for science\n");
+	printf("\t5.(SSE 2101) Fundamentals of system analysis and design \n");
+	printf("\t6.(UCU 2105) Fundamentals of ICT \n");
+	printf("\t7.(UCU 2106) Communication and Information literacy \n");
+	printf("\n");
+	printf("                  ****THE END OF UNITS****            \n");
+	return 0;
+}
