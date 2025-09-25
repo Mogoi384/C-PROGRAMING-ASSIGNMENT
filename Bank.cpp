bcc6ba30fdfb9f2f89764dbe1601f01837173979
Mogoi384 <mogoiignatius389@gmail.com>
2025-09-25 13:51:22 +0300

    Add files via upload

---
+++ Bank.cpp
@@ -0,0 +1,23 @@
+// a python program 
+/*
+Name:MOGOI IGNATIUS
+Reg no:PA106/G/28834/25
+Description:a python program showing loan application and Qualification
+*/
+
+#include<stdio.h>
+
+int main(){
+    printf("***ELIGIBILITY CHECK OF A BANK LOAN***");
+    
+    //CUSTOMERS INFORMATION
+    age=int(input("Enter your age: "))
+    annual_income=float(input("Enter your annual income: kshs."))
+    
+    //Eligibility check
+    if age=21> and annual_income=21000>:
+    	printf("Congratulations you qualify for a loan!");
+    else:
+    	printf("Unfortunately ,we are not able to offer you a loan at this time!")
+	return 0;
+}
